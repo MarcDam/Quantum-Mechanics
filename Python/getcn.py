377c9fad3ef62c7da5342b02a29cee4ac0b1255a
Marc Dam <marc.j.b.dam@gmail.com>
2017-04-17 18:58:08 +0200

    Fix norm calculation

--- Python/getcn.py
+++ Python/getcn.py
@@ -7,23 +7,25 @@ from ISW_psi_n import psi_n
 
 def getcn(x, f, epsilon = 10**(-12), n = 0):
 
-  cn = np.array(0)
+  cn = np.array([])
 
   if n == 0:
     i = 1
     
-    while np.sqrt(cn.dot(cn)) < 1 - epsilon:
+    while np.sum(np.abs(cn)**2) < 1 - epsilon:
       cn = np.append(cn, np.trapz(np.conjugate(psi_n(i, x))*f, x = x))
       i += 1
     
   else:
     i = 1
     
-    while i <= len(cn):
+    while i <= n:
       cn = np.append(cn, np.trapz(np.conjugate(psi_n(i, x))*f, x = x))
       i += 1
       
   if n == 0:
     print("Required " + str(len(cn)) + " cn's to get below " + str(epsilon) + " error")
+  else:
+    print("cn's have norm " + str(np.sum(np.abs(cn)**2))
       
   return cn
