Fix momentum spectrum; fix FSW function (not derivative)

Python/FSWBoundStates.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def hFunEven(z):
-  return np.tan(z) - np.sqrt(z0/z - 1)
+  return np.tan(z) - np.sqrt((z0/z)**2 - 1)
   
 def hFunOdd(z):
-  return -1/np.tan(z) - np.sqrt(z0/z - 1)
+  return -1/np.tan(z) - np.sqrt((z0/z)**2 - 1)
   
 hfuns = [hFunEven, hFunOdd]
 guesses = [(1, 0.5), (2, 1)]
@@ -29,7 +29,7 @@ plt.show()
 print("Newton's method:")
 
 accuracy = 10**(-6)
-guess = 1
+guess = np.pi/2-0.1
 roots = []
 
 while guess < z0:

Python/wavePackageMomentumSpectrum.py
@@ -11,7 +11,7 @@ from Psi import *
 from getcn import *
 
 x = np.linspace(0, 1, 2**10)
-t = np.linspace(0, 1/(np.pi), 2**13)
+t = np.linspace(0, 1/(2*(np.pi)), 2**10)
 
 # Wave package constants
 x0 = 0.5
@@ -44,14 +44,14 @@ for i in range(0, len(t)):
   f.append(Psi(cn, x, t[i]))
 
 # Calculate the momentum spectrum
-p = np.linspace(0, 200, 2**10)
+p = np.linspace(-200, 200, 2**8)
 pdist = []
 
 for i in range(0, len(t)):
   if i % 2**8 == 0:
     print("%.2f" %(i/len(t)))
     
-  tmp = np.zeros(p.shape)
+  tmp = np.zeros(p.shape, dtype=np.complex128)
   for j in range(0, len(p)):
     tmp[j] = np.trapz(np.conj(1/np.sqrt(2*np.pi)*np.exp(1j*p[j]*x))*f[i], x = x)
   
@@ -61,12 +61,12 @@ for i in range(0, len(t)):
 # Animate the momentum distribution using matplotlib
 
 def animate(i):
-  line.set_data(p, np.abs(pdist[i]))
+  line.set_data(p, np.abs(pdist[i])**2)
   title.set_text("t = " + str(t[i]))
   return line, title # We have to return all the objects we change
   
 fig = plt.figure()
-ax = plt.axes(xlim = (0, p[-1]), ylim = (0, 0.5))
+ax = plt.axes(xlim = (p[0], p[-1]), ylim = (0, 0.15))
 
 line, = plt.plot([], [], lw = 2)
 
@@ -76,7 +76,7 @@ anim = animation.FuncAnimation(fig, animate, frames = len(t), interval = 20, bli
 
 plt.show()
 
-anim.save("wavePackageMomentumSpectrum.mp4", writer="ffmpeg", fps = 30, codec="h264")
+anim.save("wavePackageMomentumSpectrumLowRes.mp4", writer="ffmpeg", fps = 30, codec="h264")
 
 ### MEMORY ISSUES ###
 # Use numpngw to make an animated png from the matplotlib animation