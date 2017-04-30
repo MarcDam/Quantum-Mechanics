aa822309055b6bb329e16c0942425245f3fd3b56
Marc Dam <marc.j.b.dam@gmail.com>
2017-04-30 22:24:22 +0200

    Make plot of the first four stationary states in the infinite square well

--- Python/stationarySolutions.py
+++ Python/stationarySolutions.py
@@ -6,13 +6,15 @@ import matplotlib.pyplot as plt
 # Import the stationary states
 from ISW_psi_n import *
 
-nMax = 6
+nMax = 4
 
 x = np.linspace(0, 1, num=100)
 
 for i in range(1, nMax+1):
-  plt.plot(x, psi_n(i, x), label="n = " + str(i))
+  plt.plot(x, psi_n(i, x), label="$n = " + str(i) + "$")
   
 plt.legend()
-plt.title("Stationary solutions")
-plt.show()
+plt.xlabel("$x/a$")
+plt.ylabel("$\psi_n(x)$")
+#plt.show()
+plt.savefig("ISW_stationarySolutions.png")
