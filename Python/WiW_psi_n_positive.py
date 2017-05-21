Fixed derivatives in negative psi_n; Wrote energy finding function in positive psi_n (wave functions are not right).

Python/WiW_psi_n_positive.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def psi_n(n, x):
-  E = energiesOrdered[n]
+  E = energy(n)
   
   if E[1] == "even":
     i = energies["even"].index(E[0])
@@ -29,23 +29,26 @@ def psi_n_odd(n, x):
   return np.piecewise(x, [x > b, np.logical_and(x <= b, x >= 1), np.logical_and(x <= 1, x >= -1), np.logical_and(x <= -1, x >= -b), x < -b], [0, lambda x: A*np.sinh(k*(x-b)), lambda x: D*np.sin(l*x), lambda x: -A*np.sinh(k*(-x - b)), 0])
   
 def energy(n):
+  if len(energiesOrdered) <= n:
+    findEnergies(n)
+
   return energiesOrdered[n]
   
 def hFunEven(z):
-  return z*np.tan(z)*np.tanh(np.sqrt(z0**2 - z**2) * (1 - b)) + np.sqrt(z0**2 - z**2)
+  return z*np.tan(z)*np.tan(np.sqrt(z**2 - z0**2) * (1 - b)) + np.sqrt(z**2 - z0**2)
   
 def hFunOdd(z):
-  return z*(1/np.tan(z))*np.tanh(np.sqrt(z0**2 - z**2) * (1 - b)) - np.sqrt(z0**2 - z**2)
+  return z*(1/np.tan(z))*np.tan(np.sqrt(z**2 - z0**2) * (1 - b)) - np.sqrt(z**2 - z0**2)
   
 def dhdzFunEven(z):
-  s = np.sqrt(z0**2 - z**2)
+  s = np.sqrt(z**2 - z0**2)
   
-  return np.tan(z)*np.tanh(s*(1-b)) + z * ((1/(np.cos(z))**2)*np.tanh(s*(1-b)) - (1-b)*z*np.tan(z)*(1/(s*np.cosh(s*(1-b))**2))) - z/s
+  return np.tan(z)*np.tan(s*(1-b)) + z * ((1/(np.cos(z))**2)*np.tan(s*(1-b)) + (1-b)*z*np.tan(z)*(1/(s*np.cos(s*(1-b))**2))) + z/s
  
 def dhdzFunOdd(z):
-  s = np.sqrt(z0**2 - z**2)
+  s = np.sqrt(z**2 - z0**2)
 
-  return (1/np.tan(z))*np.tanh(s*(1-b)) + z * ((-1/(np.sin(z))**2)*np.tanh(s*(1-b)) - (1-b)*z*(1/np.tan(z))*(1/(s*np.cosh(s*(1-b))**2))) + z/s
+  return (1/np.tan(z))*np.tan(s*(1-b)) + z * ((-1/(np.sin(z))**2)*np.tan(s*(1-b)) + (1-b)*z*(1/np.tan(z))*(1/(s*np.cos(s*(1-b))**2))) - z/s
   
 V0 = 13
 b = 3
@@ -54,57 +57,59 @@ z0 = np.sqrt(2*V0)
   
 hfuns = [hFunEven, hFunOdd]
 dhdzfuns = [dhdzFunEven, dhdzFunOdd]
-  
-print("Newton's method:")
 
-niter = 10**4
-guesses = [(np.pi/2 - 0.1, np.pi), (np.pi-0.1, 2)]
-newtonRoots = []
-
-for i, hfun in enumerate(hfuns):
-  print(str(hfun))
-    
-  guess = guesses[i][0]
-  newtonRoots.append([])
-  dhdz = dhdzfuns[i]
-
-  while guess < z0:
-    zi = guess
-    
-    for j in range(niter):
-      zi = zi - hfun(zi)/dhdz(zi)
-      
-    newtonRoots[i].append(zi)
-    guess = zi + guesses[i][1]
-      
-    print(zi)
-    
-# Calculate the energies and separate them into odd and even
-    
 energies = {}
 energies["even"] = []
 energies["odd"] = []
-    
-for i in range(0, len(newtonRoots[0])):
-  energies["even"].append(newtonRoots[0][i]**2/2 - V0)
-  
-for i in range(0, len(newtonRoots[1])):
-  energies["odd"].append(newtonRoots[1][i]**2/2 - V0)
-  
-# Order the energies (lowest first)
-  
 energiesOrdered = []
 
-for key in energies:
-  for i in energies[key]:
-    index = 0
-    for energy in energiesOrdered:
-      if i > energy[0]:
-        index += 1
+def findEnergies(n):
+  print("Newton's method:")
+  niter = 10**4
+  
+  if len(energies["even"]) > 0 and len(energies["odd"]) > 0:
+    guesses = [[np.sqrt(2*(energies["even"][-1] + V0)) + 0.9, 0.9, 1], [np.sqrt(2*(energies["odd"][-1] + V0)) + 0.9, 0.9, 1]]
+  else:
+    guesses = [[z0 + 0.2, 0.9, 1], [z0 + 0.1, 0.9, 1]]
+  
+  while len(energiesOrdered) < n:
+    for i, hfun in enumerate(hfuns):
+      dhdz = dhdzfuns[i]
+      zi = guesses[i][0]
+      
+      for j in range(niter):
+        zi = zi - hfun(zi)/dhdz(zi)
+      
+      print(hfun)
+      print(zi)
+      
+      E = zi**2/2 - V0
+      
+      index = 0
+      for energy in energiesOrdered:
+        if E > energy[0]:
+          index += 1
+        else:
+          break
+      
+      if i == 0:
+        energies["even"].append(E)
+        energiesOrdered.insert(index, (E, "even"))
       else:
-        break
+        energies["odd"].append(E)
+        energiesOrdered.insert(index, (E, "odd"))
         
-    energiesOrdered.insert(index, (i, key))
+      # Good guesses are hard so we tweak our guesses a little as we go
+      if i == 0 and len(energies["even"]) == 1:
+        guesses[i][0] = zi + 0.5
+      elif i == 0 and len(energies["even"]) == 2:
+        guesses[i][0] = zi + 1.3
+      elif i == 1 and len(energies["odd"]) == 1:
+        guesses[i][0] = zi + 0.6
+      elif len(energies[["even", "odd"][i]]) <= 10:
+        guesses[i][0] = zi + guesses[i][1]
+      else:
+        guesses[i][0] = zi + guesses[i][2]
 
 # Plot the bound states
 